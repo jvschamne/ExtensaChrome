98dcb0da3462db7ba9d0d90f6fada3f8f84e9044
jvschamne <schamnejv@hotmail.com>
2023-10-19 10:03:12 -0300

    commit inicial

---
+++ content.js
@@ -0,0 +1,9 @@
+
+
+url = `url("${ chrome.runtime.getURL('./Ricardom_Files.png') }");`
+
+
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'https://instagram.fbfh14-2.fna.fbcdn.net/v/t51.2885-19/334817207_697136632208145_862756621205916716_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fbfh14-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=MQXoITV1QyYAX_bVXfb&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfBccOcAhEjHp_rJOZzjLRQoEmg3yUNf-1LmzTIC20jVqg&oe=653554BB&_nc_sid=ee9879'
+}
